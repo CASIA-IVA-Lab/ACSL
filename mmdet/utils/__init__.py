b7d198d1102e73ce47e62412c3abd01bf2508ef8
changewt <1659589996@qq.com>
2021-04-02 00:08:32 +0800

    Add files via upload

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -1,4 +1,12 @@
-from .flops_counter import get_model_complexity_info
-from .registry import Registry, build_from_cfg
+from .conv_module import ConvModule, build_conv_layer
+from .conv_ws import ConvWS2d, conv_ws_2d
+from .norm import build_norm_layer
+from .scale import Scale
+from .weight_init import (bias_init_with_prob, kaiming_init, normal_init,
+                          uniform_init, xavier_init)
 
-__all__ = ['Registry', 'build_from_cfg', 'get_model_complexity_info']
+__all__ = [
+    'conv_ws_2d', 'ConvWS2d', 'build_conv_layer', 'ConvModule',
+    'build_norm_layer', 'xavier_init', 'normal_init', 'uniform_init',
+    'kaiming_init', 'bias_init_with_prob', 'Scale'
+]
